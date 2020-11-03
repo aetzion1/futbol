c3e3dffa6f9be9e4ce40284ed2bb2e31e182e388
Isabelle Villasenor <villasenori@outlook.com>
2020-11-03 09:42:22 -0700

    Add test_it_exists_with_attributes to game_teams_test

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -9,7 +9,23 @@ class GameTeamsTest < MiniTest::Test
         end
     end
 
-    def test_it_exists
+    def test_it_exists_with_attributes
         assert_instance_of GameTeams, @game_teams.first
+        assert_equal "game1", @game_teams.first.game_id
+        assert_equal "1", @game_teams.first.team_id
+        assert_equal "away", @game_teams.first.hoa
+        assert_equal "WIN", @game_teams.first.result
+        assert_equal "REG", @game_teams.first.settled_in
+        assert_equal "Coach Phil", @game_teams.first.head_coach
+        assert_equal 7, @game_teams.first.goals
+        assert_equal 8, @game_teams.first.shots
+        assert_equal 33, @game_teams.first.tackles
+        assert_equal 14, @game_teams.first.pim
+        # require 'pry';binding.pry
+        assert_equal 5, @game_teams.first.powerplayopportunities
+        assert_equal 1, @game_teams.first.powerplaygoals
+        assert_equal 37.2, @game_teams.first.faceoffwinpercentage
+        assert_equal 11, @game_teams.first.giveaways
+        assert_equal 8, @game_teams.first.takeaways
     end
 end
